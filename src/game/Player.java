9ab5bfb4079b1f902b1c8b8cb51ba1f5d0ddf49b
Josh Codd <jjc21@live.co.uk>
2020-11-15 23:37:27 +0000

    Re-do player class.

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,17 +1,38 @@
 package game;
+import java.util.ArrayList;
 
 public class Player {
+	Coord currentPosition;
+	Coord[] previousPositions;
+	int playerNumber;
+	ArrayList<ActionTile> actionTiles;
 
 
-	private Coord x;
-	private Coord y;
-	private int numOfPlayer;
-	private Player[] players;
-
-	public Player(int numOfPlayer) {
-
+	public Player(int playerNumber) {
+		this.playerNumber = playerNumber;
+		actionTiles = new ArrayList<>();
+		this.previousPositions = new Coord[2];
 	}
 
+	public void movePlayer (Coord destination) {
+		currentPosition = destination;
+	}
+
+	public void addActionTile (ActionTile tile) {
+		actionTiles.add(tile);
+	}
+
+	public void removeActionTile (int index) {
+		actionTiles.remove(index);
+	}
+
+	public ArrayList<ActionTile> getActionTiles () {
+		return actionTiles;
+	}
+
+	public Coord getPrevPosition (int index) {
+		return previousPositions[index];
+	}
 }
 	
 
