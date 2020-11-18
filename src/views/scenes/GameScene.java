ed3a5b824ed2586e8997e7b18dae4b3ed19af965
Neil Woodhouse <clonesupreme@gmail.com>
2020-11-18 12:37:41 +0000

    Ported existing Game code to Gamescene
    Merged the current work on Game and Gamescene classes.

--- src/views/scenes/GameScene.java
+++ src/views/scenes/GameScene.java
@@ -12,25 +12,48 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.FileNotFoundException;
+
 /**
- * Class that represents the main game scene.
- * @author Josh Codd
+ * Class that represents the main game scene and handles player turn sequence
+ * @author Josh Codd, Neil Woodhouse
  */
 public class GameScene {
     private GameController controller;
     private GameBoard gameBoard;
     private Stage primaryStage;
     private Player[] players;
+    private int numPlayers;
+    private TileBag tileBag = new TileBag();
+    private boolean gameOver = false;
+    private int currentPlayer = 0;
 
     /**
      * Method to construct and initialize a game scene.
      * @param stage the stage to display this scene.
      * @param game GOING TO CHANGE WHEN GAME LOGIC IS COMPLETE.
      */
-    public GameScene (Stage stage, Game game){
-        this.gameBoard = game.getGameBoard();
+    public GameScene (Stage stage, Game game,   String gameFilename, String[] playerNames){
+        numPlayers = playerNames.length;
+        players = new Player[numPlayers];
+        for (int i = 0; i < playerNames.length; i++) {
+            try {
+                PlayerProfile currentProfile = FileHandler.loadProfile(playerNames[i]);
+                players[i] = new Player(i, currentProfile);
+            } catch (FileNotFoundException e) {
+                System.out.println("Error: Player profile file not found. Please check the filepath of the game save files.");
+                //TODO Exit to level select screen
+            }
+        }
+
+        try {
+            gameBoard = FileHandler.loadNewGame(gameFilename, players, tileBag);
+        } catch(FileNotFoundException e) {
+            System.out.println("Error: The specified game file could not be found. Please check that you're providing a filepath to a valid game file location.");
+            //TODO Exit to level select scene
+        }
+
         this.primaryStage = stage;
-        this.players = game.getPlayers();
 
 
         try {
@@ -206,5 +229,20 @@ public class GameScene {
         controller.getTopButtons().getChildren().clear();
         controller.getGameBoardPane().getChildren().clear();
         controller.getBottomButtons().getChildren().clear();
+    }
+
+    /**
+     * Starts the gameplay loop. Stops when a player wins the game.
+     */
+    public void startGame() {
+
+        while (!gameOver) {
+            playerTurn(currentPlayer);
+            currentPlayer = (currentPlayer + 1) % numPlayers;
+        }
+    }
+
+    private void playerTurn(int playerNum) {
+        
     }
 }
