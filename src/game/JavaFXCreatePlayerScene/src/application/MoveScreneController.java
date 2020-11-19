6b3b7447f7d068d11e22b48827284b02f61f7e71
Josh Codd <jjc21@live.co.uk>
2020-11-19 15:42:46 +0000

    Comment out error code, to be able to run.

--- src/game/JavaFXCreatePlayerScene/src/application/MoveScreneController.java
+++ src/game/JavaFXCreatePlayerScene/src/application/MoveScreneController.java
@@ -1,42 +1,42 @@
-package application;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
-import javafx.scene.control.Button;
-
-public class MoveScreneController {
-	@FXML
-	private Label welcome;
-	
-	@FXML
-	private Button tol;
-	
-	
-	@FXML
-	public void jumpToTwoPlayerScene(ActionEvent event) {
-		MainLogic object = new MainLogic();
-		Pane view = object.jumpPlayer("ThreePlayer");
-	}
-	
-
-	
-	 
-	public void jumpToThreePlayerScene(ActionEvent event) {
-		MainLogic object = new MainLogic();
-		Pane view = object.jumpPlayer("ThreePlayer");
-		
-	}
-	
-	@FXML
-	public void jumpToFourPlayerScene(ActionEvent event) {
-		MainLogic object = new MainLogic();
-		Pane view = object.jumpPlayer("FourPlayer");
-	
-	}
-		
-}
+//package application;
+//
+//import javafx.event.ActionEvent;
+//import javafx.fxml.FXML;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
+//import javafx.scene.control.Label;
+//import javafx.scene.control.TextField;
+//import javafx.scene.layout.Pane;
+//import javafx.scene.control.Button;
+//
+//public class MoveScreneController {
+//	@FXML
+//	private Label welcome;
+//
+//	@FXML
+//	private Button tol;
+//
+//
+//	@FXML
+//	public void jumpToTwoPlayerScene(ActionEvent event) {
+//		MainLogic object = new MainLogic();
+//		Pane view = object.jumpPlayer("ThreePlayer");
+//	}
+//
+//
+//
+//
+//	public void jumpToThreePlayerScene(ActionEvent event) {
+//		MainLogic object = new MainLogic();
+//		Pane view = object.jumpPlayer("ThreePlayer");
+//
+//	}
+//
+//	@FXML
+//	public void jumpToFourPlayerScene(ActionEvent event) {
+//		MainLogic object = new MainLogic();
+//		Pane view = object.jumpPlayer("FourPlayer");
+//
+//	}
+//
+//}
