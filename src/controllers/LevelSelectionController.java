380fea97399234cbce4e594e35cd94962f13d4a5
gboumphrey <74275873+gboumphrey@users.noreply.github.com>
2020-11-25 03:18:39 +0000

    level select (buggy still)

---
+++ src/controllers/LevelSelectionController.java
@@ -0,0 +1,60 @@
+package controllers;
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.fxml.FXML;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.ButtonBase;
+import views.scenes.LevelSelectionScene;
+import javafx.scene.control.ChoiceBox;
+import java.util.ArrayList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
+import static javafx.collections.FXCollections.observableArrayList;
+import java.io.File;
+import java.util.Objects;
+import javax.swing.*;
+
+
+public class LevelSelectionController {
+
+    @FXML
+    public Button backButton;
+
+    @FXML
+    public Button confirmButton;
+
+    @FXML
+    public ChoiceBox<String> dropdown;
+
+    public void setDropdown(){
+        dropdown.setItems(getLevels());
+    }
+
+    public ObservableList<String> getLevels(){
+        File folder = new File("C:/Users/User/Documents/GitHub/Labyrinth/src/levels");
+        ObservableList<String> listOfFiles = observableArrayList();
+        System.out.println(folder.exists());
+        System.out.println(folder.isDirectory());
+        for (File i : Objects.requireNonNull(folder.listFiles())) {
+            System.out.println(i.getName());
+            listOfFiles.add(i.getName());
+        }
+        return listOfFiles;
+    }
+
+    public void handleConfirm(ActionEvent actionEvent) {
+        confirmButton.setText("doing nothing");
+    }
+
+    public void handleBack(ActionEvent actionEvent) {
+        backButton.setText("doing nothing");
+    }
+}
+
---
+++ src/views/scenes/LevelSelectionScene.java
@@ -0,0 +1,36 @@
+package views.scenes;
+
+
+import controllers.LevelSelectionController;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class LevelSelectionScene {
+    private Stage primaryStage;
+    private LevelSelectionController controller;
+
+
+    public LevelSelectionScene(Stage stage){
+        this.primaryStage = stage;
+
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            Parent root = loader.load(getClass().getClassLoader().getResource("views/layouts/LevelSelectionView.fxml").openStream());
+            Scene scene = new Scene(root, 600, 600);
+
+            LevelSelectionController controller = new LevelSelectionController();
+            controller.setDropdown();
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (Exception e){
+            e.printStackTrace();
+
+        }
+    }
+}
