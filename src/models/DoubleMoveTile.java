d9ffbcec082fdeeaddfcbd5be24d6118df999278
Josh Codd <jjc21@live.co.uk>
2020-12-04 21:35:18 +0000

    Final version

--- src/models/DoubleMoveTile.java
+++ src/models/DoubleMoveTile.java
@@ -1,14 +1,9 @@
 package models;
 /**
+ * Represents a double move tile, a form of action tile.
  * When used, the player can move twice in one turn.
  * @author George Boumphrey
  */
 public class DoubleMoveTile extends ActionTile {
-    /**
-     * Allows the player to move.
-     */
-    public void Action() {
-
-    }
 
 }
