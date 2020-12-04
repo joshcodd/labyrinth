8062ffdb1f50d1438267734bcc21aab0b3af0ad4
Josh Codd <jjc21@live.co.uk>
2020-12-04 19:56:26 +0000

    Final version

--- src/models/MessageOfTheDay.java
+++ src/models/MessageOfTheDay.java
@@ -11,6 +11,9 @@ import java.net.URLConnection;
  * @version 1.0
  */
 public class MessageOfTheDay {
+    private final String CS230_STARTING_SOLUTION = "CS-230";
+    private final int ALPHABET_LENGHT = 26;
+    private final int ASCII_VALUE = 65;
 
     // link to get encrypted text
     private static final String URL_CS230 =
@@ -20,9 +23,6 @@ public class MessageOfTheDay {
             "http://cswebcat.swansea.ac.uk/message?solution=";
     // the message of the day to be displayed on the menu
     private String message = "";
-    final private String CS230_STARTING_SOLUTION = "CS-230";
-    final private int ALPHABET_LENGHT = 26;
-    final private int ASCII_VALUE = 65;
 
     /**
      * Creates a message of the day. length
@@ -33,6 +33,15 @@ public class MessageOfTheDay {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    /**
+     * Returns the message of the day.
+     * @return The message of the day.
+     */
+    @Override
+    public String toString() {
+        return message;
     }
 
     /**
@@ -58,7 +67,7 @@ public class MessageOfTheDay {
                 shift--;
             }
         }
-        plainText += plainText.length(); // part of the solution for location the url
+        plainText += plainText.length();
         return httpRequestSend(URL_CS230_SOLUTION + plainText);
     }
 
@@ -73,25 +82,16 @@ public class MessageOfTheDay {
             throws Exception {
         String text = "";
 
-            URL url = new URL(uniformedResourceLocator);
-            URLConnection urlConnection = url.openConnection();
-            BufferedReader scan = new BufferedReader(
-                    new InputStreamReader(urlConnection.
-                            getInputStream()));
-            String line;
-            while ((line = scan.readLine()) != null) {
-                text += line;
-            }
+        URL url = new URL(uniformedResourceLocator);
+        URLConnection urlConnection = url.openConnection();
+        BufferedReader scan = new BufferedReader(
+                new InputStreamReader(urlConnection.
+                        getInputStream()));
+        String line;
+        while ((line = scan.readLine()) != null) {
+            text += line;
+        }
         return text;
-    }
-
-    /**
-     * Returns the message of the day.
-     * @return The message of the day.
-     */
-    @Override
-    public String toString() {
-        return message;
     }
 
 }
