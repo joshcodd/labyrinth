d9696dfabcb4486eca9c821585802c68d56334ab
Josh Codd <jjc21@live.co.uk>
2020-12-04 21:33:43 +0000

    Final version

--- src/models/FireTile.java
+++ src/models/FireTile.java
@@ -1,24 +1,15 @@
 package models;
 /**
- * When used, inflames tiles so players cannot step on them.
+ * Represents a fire tile, a form of action tile.
  * @author George Boumphrey
  */
 public class FireTile extends ActionTile {
     private int turnsSinceUse;
 
     /**
-     * Constructor for fire tile.
+     * Creates a fire tile.
      */
     public FireTile() {
         this.turnsSinceUse = -1;
     }
-
-
-    /**
-     * Activates the area in which the players can't enter.
-     */
-    public void Action() {
-
-    }
-    
 }
