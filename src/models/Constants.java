b605c1a1b838d5e3507d2bdd88ab67b41e6ff2fb
Josh Codd <jjc21@live.co.uk>
2020-12-04 21:43:02 +0000

    Final version

--- src/models/Constants.java
+++ src/models/Constants.java
@@ -1,13 +1,22 @@
 package models;
 
 /**
- *
- * @author
+ * Constants that are used throughout the game.
+ * @author Josh Codd
  * @version 1.0
  */
 public final class Constants {
-    private Constants (){
+    /**
+     * Cannot be created.
+     */
+    private Constants() {
     }
-    public static final int SCENE_WIDTH = 1200; //
-    public static final int SCENE_HEIGHT = 650; //
+    /**
+     * The height of the scenes throughout the game.
+     */
+    public static final int SCENE_WIDTH = 1200;
+    /**
+     * The width of the scenes throughout the game.
+     */
+    public static final int SCENE_HEIGHT = 650;
 }
