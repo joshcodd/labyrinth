f827fff3a75b5a963c451c927476f0f7b2cd9c7e
Josh Codd <jjc21@live.co.uk>
2020-12-04 21:43:59 +0000

    Final version

--- src/models/BackTrackTile.java
+++ src/models/BackTrackTile.java
@@ -1,14 +1,9 @@
 package models;
 /**
+ * Represents a back track tile, a form of action tile.
  * When used, places the player at the coords they were stood two moves ago.
  * @author George Boumphrey
  */
 public class BackTrackTile extends ActionTile {
-    /**
-     * Sends the player back to previous position from 2 turns ago.
-     */
-    public void Action() {
-
-    }
 
 }
