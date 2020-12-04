db69b1265102044f7b7d8e0f1935e4c9fbbdeb26
Josh Codd <jjc21@live.co.uk>
2020-12-04 18:50:29 +0000

    Final version

--- src/models/ActionTile.java
+++ src/models/ActionTile.java
@@ -1,31 +1,33 @@
 package models;
 
 /**
- * Super Class for IceTile, FireTile, BackTrackTile and  DoubleMoveTile
- * @author Andreas Eleftheriades   StudentID 1906277
+ * Represents an action tile.
+ * @author Andreas Eleftheriades
  * @version 1.0
  */
-public abstract class  ActionTile extends Tile {
-
-
-
-  private int turnsSinceUse = -1;
+public abstract class ActionTile extends Tile {
+    private int turnsSinceUse = -1;
 
     /**
-     * insures that activates in an area or player is effect
+     * Increments turns since use.
      */
-public abstract void Action();
-
     public void incrementTurnsSinceUse() {
         turnsSinceUse++;
     }
 
+    /**
+     * Gets the action tiles turns since use.
+     * @return The turns since use.
+     */
     public int getTurnsSinceUse() {
         return turnsSinceUse;
     }
 
+    /**
+     * Sets the action tiles turns since use.
+     * @param turns The value of turns since use.
+     */
     public void setTurnsSinceUse(int turns) {
         this.turnsSinceUse = turns;
-
     }
 }
