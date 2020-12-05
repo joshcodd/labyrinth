d531dd50ae85acd66e4c2d5404aefee2ca75f8ef
Josh Codd <jjc21@live.co.uk>
2020-12-05 15:11:17 +0000

    Final version

--- src/controllers/LeaderboardController.java
+++ src/controllers/LeaderboardController.java
@@ -1,10 +1,7 @@
 package controllers;
 
-import com.sun.deploy.util.StringUtils;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
@@ -12,168 +9,188 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.media.MediaView;
-import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import models.AudioPlayer;
 import models.FileHandler;
 import models.PlayerProfile;
 import views.scenes.MenuScene;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Objects;
-
 import static javafx.collections.FXCollections.observableArrayList;
 import static javafx.scene.paint.Color.TRANSPARENT;
-import static javafx.scene.paint.Color.WHITE;
 
 /**
- *
+ * Leader board controller.
+ * Allows the user to view the leader boards for each level.
+ * @author Josh Codd
  */
 public class LeaderboardController {
+    private final int DOT_TXT = 4;
+    private final int FORMAT_PADDING = 10;
+    //Medal images for 1st, 2nd and 3rd place.
+    private final ImageView GOLD_MEDAL
+            = new ImageView(new Image("/resources/gold.png"));
+    private final ImageView SILVER_MEDAL
+            = new ImageView(new Image("/resources/silver.png"));
+    private final ImageView BRONZE_MEDAL
+            = new ImageView(new Image("/resources/bronze.png"));
+    private final int MEDAL_SIZE = 30;
 
     @FXML
-    public Button backButton;
+    private ChoiceBox<String> dropdown;
     @FXML
-    public ChoiceBox<String> dropdown;
+    private MediaView backgroundMusic;
     @FXML
-    public MediaView backgroundMusic;
+    private ListView<HBox> leaderboard;
     @FXML
-    public ListView<HBox> leaderboard;
-    public Button muteButton;
-
-    ImageView gold = new ImageView(new Image("/resources/gold.png"));
-    ImageView silver = new ImageView(new Image("/resources/silver.png"));
-    ImageView bronze = new ImageView(new Image("/resources/bronze.png"));
-
-
-    Stage primaryStage;
+    private Button muteButton;
+    private Stage primaryStage;
 
     /**
-     *
+     * Sets the dropdown box to include all game file names.
      */
-    public void setDropdown(){
+    public void setDropdown() {
         dropdown.setItems(getLevels());
     }
 
     /**
-     * @return
+     * Handles a change in the levels dropdown box. When changed, the
+     * leaderboard also changes to whatever file name is selected.
+     * @throws FileNotFoundException If leaderboard file does not exist.
      */
-    public ObservableList<String> getLevels(){
+    public void handleChange() throws FileNotFoundException {
+        new AudioPlayer().clickPlay();
+        String gameName = dropdown.getValue();
+        leaderboard.getItems().clear();
+        if (gameName != null) { // If a name is selected
+            ArrayList<PlayerProfile> profiles = new ArrayList<>();
+            for (String player : FileHandler.loadLeaderboard(gameName)) {
+                profiles.add(FileHandler.loadProfile(player));
+            }
+            //Sort profiles by wins
+            profiles.sort((p1, p2) ->
+                    p2.getNumberOfWins() - p1.getNumberOfWins());
+
+            for (int i = 0; i < profiles.size(); i++) {
+                PlayerProfile currentProfile = profiles.get(i);
+                addLeaderboardRow(i, currentProfile);
+            }
+        }
+    }
+
+    /**
+     * Returns back to the main menu.
+     */
+    public void handleBack() {
+        new AudioPlayer().clickPlay();
+        new MenuScene(primaryStage, backgroundMusic.getMediaPlayer());
+    }
+
+    /**
+     * Mutes the background audio if its currently un-muted, and un-mutes
+     * the background audio if it is currently muted.
+     */
+    public void handleMute() {
+        backgroundMusic.getMediaPlayer()
+                .setMute(!backgroundMusic.getMediaPlayer().isMute());
+        muteButton.getStyleClass()
+                .set(0, ("mute-" + backgroundMusic.getMediaPlayer().isMute()));
+    }
+
+    /**
+     * Sets the media (audio) to be played in the background of the menu.
+     * @param backgroundMusic The audio to be played.
+     */
+    public void setBackgroundMusic(MediaView backgroundMusic) {
+        this.backgroundMusic = backgroundMusic;
+        muteButton.getStyleClass().set(0, ("mute-"
+                + backgroundMusic.getMediaPlayer().isMute()));
+    }
+
+    /**
+     * Sets the stage in which the application is being displayed on.
+     * @param primaryStage The stage being displayed on.
+     */
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    /**
+     * Gets the names of all game level files.
+     * @return All game file names.
+     */
+    private ObservableList<String> getLevels() {
         File folder = new File("src/gamefiles/levels");
         ObservableList<String> listOfFiles = observableArrayList();
         for (File i : Objects.requireNonNull(folder.listFiles())) {
-            listOfFiles.add(i.getName().substring(0, i.getName().length() - 4));
+            listOfFiles.add(i.getName().substring(0, i
+                    .getName().length() - DOT_TXT));
         }
         return listOfFiles;
     }
 
     /**
-     * @param actionEvent
-     * @throws FileNotFoundException
+     * Adds a profile to the leaderboard. If the profile is top three then
+     * their respective medal is displayed.
+     * @param index The players position in the leaderboard.
+     * @param currentProfile The profile to add to leaderboard.
      */
-    public void handleChange(ActionEvent actionEvent) throws FileNotFoundException {
-        new AudioPlayer().clickPlay();
-        String gameName = dropdown.getValue();
-        leaderboard.getItems().clear();
-        if (gameName != null){
-            ArrayList<PlayerProfile> profiles = new ArrayList<>();
-            for (String player : FileHandler.loadLeaderboard(gameName)){
-                profiles.add(FileHandler.loadProfile(player));
-            }
+    private void addLeaderboardRow(int index, PlayerProfile currentProfile) {
+        String name = currentProfile.getPlayerName();
+        String space = new String(new char[FORMAT_PADDING - name.length()])
+                .replace("\0", "0");
+        int wins = currentProfile.getNumberOfWins();
+        int losses = currentProfile.getNumberOfLosses();
+        int games = currentProfile.getNumberOfGamesPlayed();
 
-            profiles.sort(new Comparator<PlayerProfile>() {
-                public int compare(PlayerProfile p1, PlayerProfile p2) {
-                    return p2.getNumberOfWins() - p1.getNumberOfWins();
-                }
-            });
+        // Formats the players statistics
+        Text playerText = new Text(name);
+        Text whitespace = new Text(space);
+        whitespace.setFill(TRANSPARENT);
+        Text stats = new Text("| " + wins + " wins | " + losses
+                + " losses | " + games + " games played");
 
-            for (int i = 0; i < profiles.size(); i++){
-                PlayerProfile currentProfile = profiles.get(i);
+        switch (index) {
+            case 0 : // 1st = gold medal
+                GOLD_MEDAL.setFitHeight(MEDAL_SIZE);
+                GOLD_MEDAL.setFitWidth(MEDAL_SIZE);
+                playerText.getStyleClass().add("lb-text-win");
+                whitespace.getStyleClass().add("lb-text-win");
+                stats.getStyleClass().add("lb-text-win");
+                leaderboard.getItems().add(new HBox(GOLD_MEDAL,
+                        playerText, whitespace, stats));
+                break;
 
+            case 1 : // 2nd = silver medal
+                SILVER_MEDAL.setFitHeight(MEDAL_SIZE);
+                SILVER_MEDAL.setFitWidth(MEDAL_SIZE);
+                whitespace.getStyleClass().add("lb-text-win");
+                stats.getStyleClass().add("lb-text-win");
+                playerText.getStyleClass().add("lb-text-win");
+                leaderboard.getItems().add(new HBox(SILVER_MEDAL,
+                        playerText, whitespace, stats));
+                break;
 
-                String name = currentProfile.getPlayerName();
-                String space = new String(new char[10 - name.length()]).replace("\0", "0");
-                int wins = currentProfile.getNumberOfWins();
-                int losses = currentProfile.getNumberOfLosses();
-                int games = currentProfile.getNumberOfGamesPlayed();
+            case 2 : // 3rd = bronze medal
+                BRONZE_MEDAL.setFitHeight(MEDAL_SIZE);
+                BRONZE_MEDAL.setFitWidth(MEDAL_SIZE);
+                whitespace.getStyleClass().add("lb-text-win");
+                stats.getStyleClass().add("lb-text-win");
+                playerText.getStyleClass().add("lb-text-win");
+                leaderboard.getItems().add(new HBox(BRONZE_MEDAL,
+                        playerText, whitespace, stats));
+                break;
 
-                Text playerText = new Text(name);
-                Text whitespace = new Text(space);
-                whitespace.setFill(TRANSPARENT);
-                Text stats = new Text("| " + wins + " wins | " + losses + " losses | " + games + " games played");
-
-                switch (i){
-                    case 0 :
-                        gold.setFitHeight(30);
-                        gold.setFitWidth(30);
-                        playerText.getStyleClass().add("lb-text-win");
-                        whitespace.getStyleClass().add("lb-text-win");
-                        stats.getStyleClass().add("lb-text-win");
-                        leaderboard.getItems().add(new HBox(gold, playerText, whitespace,stats));
-                        break;
-
-                    case 1 :
-                        silver.setFitHeight(30);
-                        silver.setFitWidth(30);
-                        whitespace.getStyleClass().add("lb-text-win");
-                        stats.getStyleClass().add("lb-text-win");
-                        playerText.getStyleClass().add("lb-text-win");
-                        leaderboard.getItems().add(new HBox(silver, playerText, whitespace,stats));
-                        break;
-
-                    case 2 :
-                        bronze.setFitHeight(30);
-                        bronze.setFitWidth(30);
-                        whitespace.getStyleClass().add("lb-text-win");
-                        stats.getStyleClass().add("lb-text-win");
-                        playerText.getStyleClass().add("lb-text-win");
-                        leaderboard.getItems().add(new HBox(bronze, playerText, whitespace,stats));
-                        break;
-                    default:
-                        whitespace.getStyleClass().add("lb-text");
-                        stats.getStyleClass().add("lb-text");
-                        playerText.getStyleClass().add("lb-text");
-                        HBox cell = new HBox(playerText, whitespace,stats);
-                        cell.setPadding(new Insets(5,0,5,30));
-                        leaderboard.getItems().add(cell);
-                }
-            }
+            default: // Non top three row.
+                whitespace.getStyleClass().add("lb-text");
+                stats.getStyleClass().add("lb-text");
+                playerText.getStyleClass().add("lb-text");
+                HBox cell = new HBox(playerText, whitespace, stats);
+                cell.getStyleClass().add("leaderboard-cell");
+                leaderboard.getItems().add(cell);
         }
     }
-
-    /**
-     * @param actionEvent
-     */
-    public void handleBack(ActionEvent actionEvent) {
-        new AudioPlayer().clickPlay();
-        MenuScene menuScene = new MenuScene(primaryStage, backgroundMusic.getMediaPlayer());
-    }
-
-    /**
-     * @param actionEvent
-     */
-    public void handleMute(ActionEvent actionEvent) {
-        backgroundMusic.getMediaPlayer().setMute(!backgroundMusic.getMediaPlayer().isMute());
-        muteButton.getStyleClass().set(0, ("mute-" + backgroundMusic.getMediaPlayer().isMute()));
-    }
-
-    /**
-     * @param backgroundMusic
-     */
-    public void setBackgroundMusic(MediaView backgroundMusic) {
-        this.backgroundMusic = backgroundMusic;
-        muteButton.getStyleClass().set(0, ("mute-" + backgroundMusic.getMediaPlayer().isMute()));
-    }
-
-    /**
-     * @param primaryStage
-     */
-    public void setPrimaryStage(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-    }
 }
-
