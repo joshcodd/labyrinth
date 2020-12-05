e94199922e67fc1d0d9249f07b3757f51f8a9b5e
Josh Codd <jjc21@live.co.uk>
2020-12-05 14:40:56 +0000

    Final version

--- src/controllers/LevelSelectionController.java
+++ src/controllers/LevelSelectionController.java
@@ -2,17 +2,14 @@ package controllers;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
 import javafx.fxml.FXML;
-import javafx.event.ActionEvent;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 import models.AudioPlayer;
 import views.scenes.MenuScene;
 import views.scenes.SelectPlayerScene;
-
 import static javafx.collections.FXCollections.observableArrayList;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Objects;
 
 /**
@@ -21,70 +18,52 @@ import java.util.Objects;
  * @author George Boumphrey
  */
 public class LevelSelectionController {
-
+    private final int DOT_TXT = 4;
     @FXML
-    public Button backButton;
-
+    private ChoiceBox<String> dropdown;
     @FXML
-    public Button confirmButton;
-
+    private MediaView backgroundMusic;
     @FXML
-    public ChoiceBox<String> dropdown;
-    @FXML
-    public MediaView backgroundMusic;
-    public Button muteButton;
+    private Button muteButton;
 
     Stage primaryStage;
 
     /**
-     * Method to set choices for which level to select.
+     * Sets choices for which level to select.
      */
-    public void setDropdown(){
+    public void setDropdown() {
         dropdown.setItems(getLevels());
     }
 
     /**
-     * Create a list of level names from the directory containing level files.
-     * @return A list of level file names.
-     */
-    public ObservableList<String> getLevels(){
-        File folder = new File("src/gamefiles/levels");
-        ObservableList<String> listOfFiles = observableArrayList();
-        for (File i : Objects.requireNonNull(folder.listFiles())) {
-            listOfFiles.add(i.getName().substring(0, i.getName().length() - 4));
-        }
-        return listOfFiles;
-    }
-
-    /**
      * Progress to next scene on button press.
-     * @param actionEvent
-     * @throws FileNotFoundException
      */
-    public void handleConfirm(ActionEvent actionEvent) throws FileNotFoundException {
+    public void handleConfirm() {
         new AudioPlayer().clickPlay();
         String gameName = dropdown.getValue();
-        if (gameName != null){
-            new SelectPlayerScene(primaryStage, dropdown.getValue(), backgroundMusic.getMediaPlayer());
+        if (gameName != null) {
+            new SelectPlayerScene(primaryStage, dropdown.getValue(),
+                    backgroundMusic.getMediaPlayer());
         }
     }
 
     /**
-     * Return to main menu on button press.
-     * @param actionEvent
+     * Mutes the background audio if its currently un-muted, and un-mutes
+     * the background audio if it is currently muted.
      */
-    public void handleBack(ActionEvent actionEvent) {
+    public void handleBack() {
         new AudioPlayer().clickPlay();
-        MenuScene menuScene = new MenuScene(primaryStage, backgroundMusic.getMediaPlayer());
+        new MenuScene(primaryStage, backgroundMusic.getMediaPlayer());
     }
 
     /**
      * Mute background music on button press.
-     * @param actionEvent
      */
-    public void handleMute(ActionEvent actionEvent) {
-        backgroundMusic.getMediaPlayer().setMute(!backgroundMusic.getMediaPlayer().isMute());
-        muteButton.getStyleClass().set(0, ("mute-" + backgroundMusic.getMediaPlayer().isMute()));
+    public void handleMute() {
+        backgroundMusic.getMediaPlayer()
+                .setMute(!backgroundMusic.getMediaPlayer().isMute());
+        muteButton.getStyleClass().set(0, ("mute-"
+                + backgroundMusic.getMediaPlayer().isMute()));
     }
 
     /**
@@ -93,7 +72,8 @@ public class LevelSelectionController {
      */
     public void setBackgroundMusic(MediaView backgroundMusic) {
         this.backgroundMusic = backgroundMusic;
-        muteButton.getStyleClass().set(0, ("mute-" + backgroundMusic.getMediaPlayer().isMute()));
+        muteButton.getStyleClass().set(0, ("mute-"
+                + backgroundMusic.getMediaPlayer().isMute()));
     }
 
     /**
@@ -103,5 +83,18 @@ public class LevelSelectionController {
     public void setPrimaryStage(Stage primaryStage) {
         this.primaryStage = primaryStage;
     }
-}
 
+    /**
+     * Create a list of level names from the directory containing level files.
+     * @return All level file names.
+     */
+    private ObservableList<String> getLevels() {
+        File folder = new File("src/gamefiles/levels");
+        ObservableList<String> listOfFiles = observableArrayList();
+        for (File i : Objects.requireNonNull(folder.listFiles())) {
+            listOfFiles.add(i.getName().substring(0, i.getName()
+                    .length() - DOT_TXT));
+        }
+        return listOfFiles;
+    }
+}
